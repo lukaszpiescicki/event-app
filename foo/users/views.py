6b010553fc26d8c22a34b2e646051fe3e0aba684
Lukasz <lukasz.piescicki@gmail.com>
2024-05-12 10:45:01 +0200

    refactor: replaced function based views with class based views

--- foo/users/views.py
+++ foo/users/views.py
@@ -1,56 +1,62 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import FormView, UpdateView, View
 
-from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm
+from .forms import UserRegisterForm, UserUpdateForm
 from .models import CustomUser, FriendRequest
 
 
-def register(request):
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(
-                request, f"Dear {username}, you have successfully signed up!"
-            )
-            return redirect("login")
-    else:
-        form = UserRegisterForm()
+class UserRegisterView(FormView):
+    template_name = "users/register.html"
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("login")
 
-    return render(request, "users/register.html", context={"form": form})
+    def form_valid(self, form):
+        form.save()
+        username = form.cleaned_data.get("username")
+        messages.success(
+            self.request, f"Dear {username}, you have successfully signed up!"
+        )
+        return super().form_valid(form)
 
 
-@login_required
-def profile(request):
-    if request.method == "POST":
-        user_form = UserUpdateForm(request.POST, instance=request.user)
+class ProfileUpdateView(UpdateView, LoginRequiredMixin):
+    model = User
+    form_class = UserUpdateForm
+    template_name = "users/profile.html"
+    success_url = reverse_lazy("profile")
 
-        if user_form.is_valid():
-            user_form.save()
-            messages.success(request, "Your user's been updated!")
-            return redirect(profile)
+    def get_object(self, queryset=None) -> User:
+        return self.request.user
 
-    return render(request, "users/profile.html", {"user_form": UserUpdateForm()})
+    def form_valid(self, form):
+        messages.success(self.request, "Your user's been updated!")
+        return super().form_valid(form)
 
-@login_required
-def send_friend_request(request, user_id):
-    from_user = request.user
-    to_user = CustomUser.objects.filter(id=user_id)
-    friend_request, created = FriendRequest.objects.get_or_create(from_user=from_user, to_user=to_user)
-    if created:
-        return HttpResponse('friend request sent')
-    return HttpResponse('friend request was already sent')
 
-@login_required
-def accept_friend_request(request, request_id):
-    friend_request = FriendRequest.objects.get(id=request_id)
-    if friend_request.to_user == request.user:
-        friend_request.to_user.friends.add(friend_request.from_user)
-        friend_request.from_user.friends.add(friend_request.to_user)
-        friend_request.delete()
-        return HttpResponse('friend request accepted')
-    else:
-        return HttpResponse('friend request not accepted')
+class SendFriendRequestView(View):
+    def get(self, request, user_id):
+        from_user = request.user
+        to_user = get_object_or_404(CustomUser, id=user_id)
+        friend_request, created = FriendRequest.objects.get_or_create(
+            from_user=from_user, to_user=to_user
+        )
+        if created:
+            return HttpResponse("friend request sent")
+        return HttpResponse("friend request was already sent")
+
+
+class AcceptFriendRequestView(View):
+    def get(self, request, request_id):
+        friend_request = get_object_or_404(FriendRequest, pk=request_id)
+        if friend_request.to_user == request.user:
+            friend_request.to_user.friends.add(friend_request.from_user)
+            friend_request.from_user.friends.add(friend_request.to_user)
+            friend_request.delete()
+            return HttpResponse("friend request accepted")
+
+        return HttpResponse("friend request not accepted")
